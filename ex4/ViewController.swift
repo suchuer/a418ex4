59728bd6064505e8824683ad14b0324caf1c6eab
suchuer <chi.chuer@msa.hinet.net>
2021-04-18 16:21:33 +0800

    note

--- ex4/ViewController.swift
+++ ex4/ViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var displayLable: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let def = UserDefaults.standard
+        let value = def.value(forKey: "lastInputDate")
+        if let date = value as? Date{
+            let formater = DateFormatter()
+            formater.dateFormat = "yyyy-MM-dd HH:mm"
+            formater.locale = Locale.init(identifier: "zh-tw")
+            let dateString = formater.string(from: date)
+            displayLable.text = dateString
+        }
     }
 
-
+    @IBAction func dateselect(_ sender: UIDatePicker) {
+        let dateselect = sender.date
+        //print(dateselect.timeIntervalSince1970)
+        
+        let def = UserDefaults.standard
+        def.setValue(dateselect, forKey: "lastInputDate")
+        def.synchronize()
+        
+        //text
+        let formater = DateFormatter()
+        formater.dateFormat = "yyyy-MM-dd HH:mm"
+        formater.locale = Locale.init(identifier: "zh-tw")
+        let dateString = formater.string(from: dateselect)
+        displayLable.text = dateString
+    }
+    
 }
 
